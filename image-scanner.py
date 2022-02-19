b496477e27f7cad0f33c0726269c6bba97595c9d
Mahmudul Hasan <cs.mahmud@gmail.com>
2022-02-18 18:11:15 -0700

    Added OpenCV processing

--- image-scanner.py
+++ image-scanner.py
@@ -1,11 +1,38 @@
 import gi
 
+import cv2 as cv2
+import numpy as np
+
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
+from gi.repository import GdkPixbuf
 from gi.repository.GdkPixbuf import Pixbuf
 from gi.repository.GdkPixbuf import InterpType
 from gi.repository.GdkPixbuf import PixbufRotation
 from gi.repository import GLib
+
+
+def cv_to_pixbuf(img):
+    img_rgb =  cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    print ("Loading image: " , img.shape)
+    print("Depth : ", img.dtype.itemsize, img.strides)
+    height, width,depth = img.shape
+    r_s, c_s, d_s = img.strides
+    pixbuf = Pixbuf.new_from_data(img_rgb.tobytes(),
+                                  GdkPixbuf.Colorspace.RGB,
+                                  False,
+                                  img.dtype.itemsize * 8,
+                                  width,
+                                  height,
+                                  r_s)
+    return pixbuf
+
+
+def scale_pixbuf(pixbuf_in, ratio): 
+    pixbuf_scaled = pixbuf_in.scale_simple(pixbuf_in.get_width()/ratio, 
+                                           pixbuf_in.get_height()/ratio,
+                                           InterpType.HYPER)
+    return pixbuf_scaled
 
 
 class MyWindow(Gtk.Window):
@@ -39,7 +66,7 @@ class MyWindow(Gtk.Window):
         top_panel.pack_start(button, False, False, 0)
 
         button = Gtk.Button(label="CV")
-        button.connect("clicked", self.on_button_clicked)
+        button.connect("clicked", self.on_cv)
         top_panel.pack_start(button, False, False, 0)        
 
         button = Gtk.Button(label="Save")
@@ -69,11 +96,11 @@ class MyWindow(Gtk.Window):
 
     def on_load(self, widget):
         self.ratio = 5
-        self.pixbuf_source = Pixbuf.new_from_file("IMG_1.JPG")
+        # self.pixbuf_source = Pixbuf.new_from_file("IMG_1.JPG")
         
-        self.pixbuf_scaled = self.pixbuf_source.scale_simple(self.pixbuf_source.get_width()/self.ratio,
-                                                        self.pixbuf_source.get_height()/self.ratio,
-                                                        InterpType.HYPER)
+        self.img = cv2.imread('IMG_1.JPG',1)
+        self.pixbuf_source = cv_to_pixbuf(self.img)
+        self.pixbuf_scaled = scale_pixbuf(self.pixbuf_source, self.ratio)
 
         image = Gtk.Image.new_from_pixbuf(self.pixbuf_scaled)
 
@@ -107,6 +134,31 @@ class MyWindow(Gtk.Window):
     def on_save(self, widget):
         if self.pixbuf_source is not None:
             self.pixbuf_source.savev("output.jpg", "jpeg", [] , [] )
+
+
+    def on_cv(self, widget):
+        rgb_planes = cv2.split(self.img)
+
+        result_planes = []
+        result_norm_planes = []
+
+        for plane in rgb_planes:
+            dilated_img = cv2.dilate(plane, np.ones((7,7), np.uint8))
+            bg_img = cv2.medianBlur(dilated_img, 21)
+            diff_img = 255 - cv2.absdiff(plane, bg_img)
+            norm_img = cv2.normalize(diff_img,None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
+            result_planes.append(diff_img)
+            result_norm_planes.append(norm_img)
+
+        result = cv2.merge(result_planes)
+        result_norm = cv2.merge(result_norm_planes)
+
+        self.img = result
+        
+        self.pixbuf_source = cv_to_pixbuf(self.img)
+        self.pixbuf_scaled = scale_pixbuf(self.pixbuf_source, self.ratio)
+        self.pixbuf_copy = self.pixbuf_scaled.copy()        
+        self.update_image()
 
         
         
