f5e2c6469a2fe3bc3551d6505fe789bf7114990a
Mahmudul Hasan <cs.mahmud@gmail.com>
2022-02-28 00:09:27 -0700

    Added a util file

---
+++ image_utils.py
@@ -0,0 +1,26 @@
+import cv2 as cv2
+
+def cv_to_pixbuf(img):
+    img_rgb =  cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    print ("Loading image: " , img.shape)
+    print("Depth : ", img.dtype.itemsize, img.strides)
+    height, width,depth = img.shape
+    pixbuf = Pixbuf.new_from_data(img_rgb.tobytes(),
+                                  GdkPixbuf.Colorspace.RGB,
+                                  False,
+                                  img.dtype.itemsize * 8,
+                                  width,
+                                  height,
+                                  width * 3)
+    return pixbuf
+
+def cv_resize(img, ratio):
+    w, h,d  = img.shape
+    width = int(w * ratio)
+    height = int (h * ratio)
+    dim = (height, width)
+
+    print ("ratio " , ratio)
+    print ("resizing from " , img.shape, " => " , dim)
+    resized = cv2.resize(img, None, fx=ratio, fy=ratio , interpolation = cv2.INTER_AREA)
+    return resized
--- rectangle.py
+++ rectangle.py
@@ -17,9 +17,16 @@ from gi.repository.GdkPixbuf import InterpType
 from gi.repository.GdkPixbuf import PixbufRotation
 from gi.repository import GLib
 
+from image_utils import cv_resize
+from image_utils import cv_to_pixbuf
+
 class MyWindow(Gtk.Window):
     def __init__(self):
         super().__init__(title="Hello World")
+        self.current_image = None
+        self.img_src = None
+        
+        self.ratio = 1.0
 
         root_panel = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
         top_panel = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
@@ -35,7 +42,12 @@ class MyWindow(Gtk.Window):
         self.add(root_panel)
 
     def on_load(self, widget):
-        print ("Hello World")
+        self.current_image = "/home/p-hasan/work/math4310/hw3/out/IMG_1133.JPG"
+        self.img_src = cv2.imread(self.current_image,1)
+        self.ratio = .20
+
+        self.img = cv_resize(self.img_src, self.ratio)
+
 
 
 win = MyWindow()
